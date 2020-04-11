956dbdea8cd49f68cbeb19d18349cb48b2929608
Nick <18233355+Kandrid@users.noreply.github.com>
2020-04-11 19:02:17 -0300

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -11,51 +11,16 @@ namespace GameEngineV2
 
         public void Initialise()
         {
-            LoadMap("space.cmap");
-            FRAMERATE = 1000;
-            DEBUG_MODE = Debug.Partial;
-            CAMERA.Width = 150;
-            CAMERA.Height = 100;
-            new Entity('o', 8, 3, 1f, 0.1f);
-        }
-
-        struct Data
-        {
-            public float x, y, Vx, Vy;
-
-            public Data(float x, float y, float Vx, float Vy)
-            {
-                this.x = x;
-                this.y = y;
-                this.Vx = Vx;
-                this.Vy = Vy;
-            }
+            LoadMap("large.cmap");
+            FRAMERATE = 24;
+            DEBUG_MODE = Debug.Full;
         }
 
         //Called every frame
 
         public void GameExecute()
         {
-            HashSet<Data> splits = new HashSet<Data>();
             
-            foreach (Entity e in Entity.Entities)
-            {
-                if (e.ExactX + e.Vx < 1 || e.ExactX + e.Vx >= MAP_WIDTH - 1)
-                {
-                    splits.Add(new Data(e.ExactX, e.ExactY, -e.Vx, -e.Vy));
-                    e.Vx *= -1;
-                }
-                if (e.ExactY + e.Vy < 1 || e.ExactY + e.Vy >= MAP_HEIGHT - 1)
-                {
-                    splits.Add(new Data(e.ExactX, e.ExactY, -e.Vx, -e.Vy));
-                    e.Vy *= -1;
-                }
-            }
-
-            foreach (Data d in splits)
-            {
-                new Entity('o', d.x, d.y, d.Vx, d.Vy);
-            }
         }
     }
 }
